9996894d799742b7907b93f2330509d69f510346
CristianN01 <cristiannechifor71@gmail.com>
2024-05-23 16:39:10 +0200

    risolto il primo snack, creato la lista di invitati al tavolo vip, poi creato il ciclo per stamparli in console con il loro segnaposto

---
+++ SNACK 1/JS/script.js
@@ -0,0 +1,61 @@
+const tavoloVip = [
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Brad Pitt',
+        posto :'1',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Johnny Depp',
+        posto :'2',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Lady Gaga',
+        posto :'3',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Cristiano Ronaldo',
+        posto :'4',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Georgina Rodriguez',
+        posto :'5',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Chiara Ferragni',
+        posto :'6',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'George Clooney',
+        posto :'7',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Amal Clooney',
+        posto :'8',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Fedez',
+        posto :'9',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Amadeus',
+        posto :'10',
+    },
+    {
+        nomeTavolo : 'Tavolo Vip',
+        nome : 'Fiorello',
+        posto :'11',
+    },
+]
+
+tavoloVip.forEach((element) => {
+    console.log( `${element.nome}, ${element.nomeTavolo} il suo segna posto e ${element.posto}`);
+});
