bcb18e7d352562fa797a2a3bf19b87f5d2325f15
CristianN01 <cristiannechifor71@gmail.com>
2024-05-23 16:56:39 +0200

    creata l`array aula con gli studenti, e le loro proprieta

---
+++ SNACK 2/JS/script.js
@@ -0,0 +1,37 @@
+const aula = [
+    {
+        Id :'213',
+        Name :'Giuseppina della Rovere',
+        Grades :'78',
+    },
+    {
+        Id :'110',
+        Name :'aola Cortellessa',
+        Grades :'96',
+    },
+    {
+        Id :'250',
+        Name :'Andrea Mantegna',
+        Grades :'48',
+    },
+    {
+        Id :'145',
+        Name :'Gaia Borromini',
+        Grades :'74',
+    },
+    {
+        Id :'196',
+        Name :'Luigi Grimaldello',
+        Grades :'68',
+    },
+    {
+        Id :'102',
+        Name :'Piero della Francesca',
+        Grades :'50',
+    },
+    {
+        Id :'120',
+        Name :'Francesca da Polenta',
+        Grades :'84',
+    },
+]
