b836edd060091a75424628a70871c1c1c8ec2de8
CristianN01 <cristiannechifor71@gmail.com>
2024-05-23 17:42:10 +0200

    creato ultimo ciclo che stampa in un nuovo array gli studenti con Id superiore a 120 e valutazione superiore a 70.

--- SNACK 2/JS/script.js
+++ SNACK 2/JS/script.js
@@ -1,36 +1,36 @@
 const aula = [
     {
-        Id :'213',
+        Id :213,
         Name :'Giuseppina della Rovere',
         Grades :78,
     },
     {
-        Id :'110',
+        Id :110,
         Name :'aola Cortellessa',
         Grades :96,
     },
     {
-        Id :'250',
+        Id :250,
         Name :'Andrea Mantegna',
         Grades :48,
     },
     {
-        Id :'145',
+        Id :145,
         Name :'Gaia Borromini',
         Grades :74,
     },
     {
-        Id :'196',
+        Id :196,
         Name :'Luigi Grimaldello',
         Grades :68,
     },
     {
-        Id :'102',
+        Id :102,
         Name :'Piero della Francesca',
         Grades :50,
     },
     {
-        Id :'120',
+        Id :120,
         Name :'Francesca da Polenta',
         Grades :84,
     },
@@ -51,6 +51,16 @@ const bestStudents = aula.filter((element) => {
 });
 
 console.log(bestStudents);
+
+const idBestStudents = aula.filter((element) => {
+    if(element.Grades > 70 && element.Id >= 120){
+        return true;
+    }
+});
+
+console.log(idBestStudents);
+
+
 
 
 
