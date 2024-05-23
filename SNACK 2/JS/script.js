66bed7ab560fadb529ef9b301104c7c67a389fd9
CristianN01 <cristiannechifor71@gmail.com>
2024-05-23 17:38:47 +0200

    creato ciclo che filtra la lista e ne crea un altra con solo gli studenti con voti superiori a 70

--- SNACK 2/JS/script.js
+++ SNACK 2/JS/script.js
@@ -2,42 +2,55 @@ const aula = [
     {
         Id :'213',
         Name :'Giuseppina della Rovere',
-        Grades :'78',
+        Grades :78,
     },
     {
         Id :'110',
         Name :'aola Cortellessa',
-        Grades :'96',
+        Grades :96,
     },
     {
         Id :'250',
         Name :'Andrea Mantegna',
-        Grades :'48',
+        Grades :48,
     },
     {
         Id :'145',
         Name :'Gaia Borromini',
-        Grades :'74',
+        Grades :74,
     },
     {
         Id :'196',
         Name :'Luigi Grimaldello',
-        Grades :'68',
+        Grades :68,
     },
     {
         Id :'102',
         Name :'Piero della Francesca',
-        Grades :'50',
+        Grades :50,
     },
     {
         Id :'120',
         Name :'Francesca da Polenta',
-        Grades :'84',
+        Grades :84,
     },
 ]
 
 const upperAula = aula.map((name) => {
-    return name.Name.toUpperCase()
+    return `${name.Name.toUpperCase()}: il suo Id e ${name.Id}, uscito con ${name.Grades}.`
 });
 
 console.log (upperAula);
+
+
+
+const bestStudents = aula.filter((element) => {
+    if(element.Grades > 70){
+        return true;
+    }
+});
+
+console.log(bestStudents);
+
+
+
