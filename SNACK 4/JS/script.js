714a044b986ceafd9077a165e79016d6e483b9d8
CristianN01 <cristiannechifor71@gmail.com>
2024-05-27 18:30:14 +0200

    creato array con le mie squadre e le loro proprieta

---
+++ SNACK 4/JS/script.js
@@ -0,0 +1,29 @@
+const squadreDiCalcio = [
+    {
+      nome: 'Juventus',
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: 'Milan',
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: 'Inter',
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: 'Roma',
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: 'Napoli',
+      puntiFatti: 0,
+      falliSubiti: 0
+    }
+  ];
+  
+  console.log(squadreDiCalcio);
